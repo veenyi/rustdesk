ed9a69e4d151ff81da95ab4eebd0f1d1854d221c
veenyi <60565189+veenyi@users.noreply.github.com>
2024-10-10 16:17:29 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,7 +100,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["nas.aio.run"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["nas.aio.run:21116"];
 pub const PUBLIC_RS_PUB_KEY: &str = "ERuFPEdpFv3oRvAz36hTo+gPJnCpz8EAL1Jgl3FdLhk=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
